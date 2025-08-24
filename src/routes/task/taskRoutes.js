b5fa21b84961c1e77547686483ae90c226f32278
Schandroid243 <schadrackngunza@gmail.com>
2025-08-24 13:24:02 +0100

    feat(validators/authValidation, taskValidation): Added request validation using joi  to make sure only correct data enters our system. feat(middlewares/validate): Prevents broken/malicious input.

--- src/routes/task/taskRoutes.js
+++ src/routes/task/taskRoutes.js
@@ -7,14 +7,20 @@ const {
   deleteTask,
 } = require('./taskController');
 const auth = require('../../middlewares/authMiddleware').auth;
+const validate = require('../../middlewares/validate');
+const {
+  createTaskSchema,
+  updateTaskSchema,
+} = require('../../validators/taskValidation');
 
 const router = express.Router();
 
-// All routes protected
-router.post('/', auth, createTask);
-router.get('/', auth, getTasks);
-router.get('/:id', auth, getTask);
-router.put('/:id', auth, updateTask);
-router.delete('/:id', auth, deleteTask);
+router.use(auth); // protect all task routes
+
+router.post('/', validate(createTaskSchema), createTask);
+router.get('/', getTasks);
+router.get('/:id', getTask);
+router.put('/:id', validate(updateTaskSchema), updateTask);
+router.delete('/:id', deleteTask);
 
 module.exports = router;
